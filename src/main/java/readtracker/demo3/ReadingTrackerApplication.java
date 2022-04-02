b2fb031534170401dbd8e98bcb3b62d8b2abc444
ella.tomlinson <ella.tomlinson@ucalgary.ca>
2022-04-02 14:33:23 -0600

    Fixed window size

--- src/main/java/readtracker/demo3/ReadingTrackerApplication.java
+++ src/main/java/readtracker/demo3/ReadingTrackerApplication.java
@@ -11,8 +11,8 @@ public class ReadingTrackerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ReadingTrackerApplication.class.getResource("readingTracker-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Reading Tracker");
         stage.setScene(scene);
         stage.show();
     }
