c12e151ffa9e1a21ea082a106e096a94eaaedba1
ella.tomlinson <ella.tomlinson@ucalgary.ca>
2022-04-02 12:38:34 -0600

    Started outlining GUI structure

--- src/main/java/readtracker/demo3/ReadingTrackerController.java
+++ src/main/java/readtracker/demo3/ReadingTrackerController.java
@@ -2,13 +2,49 @@ package readtracker.demo3;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class ReadingTrackerController {
     @FXML
-    private Label welcomeText;
+    private Font x1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Font x11;
+
+    @FXML
+    private Font x111;
+
+    @FXML
+    private Font x12;
+
+    @FXML
+    private Font x13;
+
+    @FXML
+    private Font x131;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Color x21;
+
+    @FXML
+    private Color x211;
+
+    @FXML
+    private Color x22;
+
+    @FXML
+    private Color x23;
+
+    @FXML
+    private Color x231;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
 }
