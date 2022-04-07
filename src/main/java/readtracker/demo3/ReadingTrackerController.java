273444d7bfcbe08277a7ffcee90940b3e607c4d4
ella.tomlinson <ella.tomlinson@ucalgary.ca>
2022-04-07 14:28:43 -0600

    Deleted unnecessary variables

--- src/main/java/readtracker/demo3/ReadingTrackerController.java
+++ src/main/java/readtracker/demo3/ReadingTrackerController.java
@@ -3,8 +3,6 @@ package readtracker.demo3;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
