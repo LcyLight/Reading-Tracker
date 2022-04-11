fd9500505db040d04a38ca8439bd7f3ef52bbf32
ella.tomlinson <ella.tomlinson@ucalgary.ca>
2022-04-11 14:51:30 -0600

    Made JUnit test file

---
+++ src/Test/readtracker/demo3/ReadingTrackerMainTest.java
@@ -0,0 +1,52 @@
+package readtracker.demo3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadingTrackerMainTest {
+
+    @Test
+    void bookLogView() {
+    }
+
+    @Test
+    void readingListView() {
+    }
+
+    @Test
+    void logInfoView() {
+    }
+
+    @Test
+    void listInfoView() {
+    }
+
+    @Test
+    void allInfoView() {
+    }
+
+    @Test
+    void getRec() {
+    }
+
+    @Test
+    void rateSortString() {
+    }
+
+    @Test
+    void stringRatingView() {
+    }
+
+    @Test
+    void stringMonthStats() {
+    }
+
+    @Test
+    void stringGenreStats() {
+    }
+
+    @Test
+    void runFromArgs() {
+    }
+}
