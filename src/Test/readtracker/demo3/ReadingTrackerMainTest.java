a5cffaae323ea52d61b5d007eba0965e91ba2d04
yiting.ouyang <yiting.ouyang@cs.ucalgary.ca>
2022-04-12 00:49:31 +0000

    added test for bookLog view

--- src/Test/readtracker/demo3/ReadingTrackerMainTest.java
+++ src/Test/readtracker/demo3/ReadingTrackerMainTest.java
@@ -2,12 +2,23 @@ package readtracker.demo3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReadingTrackerMainTest {
 
     @Test
     void bookLogView() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        BookLogItem item = new BookLogItem("Goodnight Moon", "Tim Orwell", "February", 4, 534, "fantasy");
+
+        bookLog.put("Goodnight Moon", item);
+
+        String myString = String.valueOf(ReadingTrackerMain.bookLogView(bookLog));
+
+        assertEquals("Goodnight Moon\n",myString);
     }
 
     @Test
