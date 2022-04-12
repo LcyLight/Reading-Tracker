d6c60f92937a69ea0885ef826e89956377fd1dd6
yiting.ouyang <yiting.ouyang@cs.ucalgary.ca>
2022-04-12 03:52:38 +0000

    finished tests for listInfoView

--- src/main/java/readtracker/demo3/ReadingTrackerController.java
+++ src/main/java/readtracker/demo3/ReadingTrackerController.java
@@ -125,8 +125,78 @@ class ReadingTrackerMainTest {
 
 
     @Test
-    void listInfoView() {
+    void listInfoViewNoBooks() {
+
+        HashMap<String, ReadingListItem> readingList = new HashMap<>();
+
+        String outputString = String.valueOf(ReadingTrackerMain.listInfoView(readingList));
+
+        assertEquals("You have no books in your Reading List!", outputString);
+
     }
+
+    @Test
+    void listInfoView1Book() {
+
+        HashMap<String, ReadingListItem> readingList = new HashMap<>();
+
+        ReadingListItem item1 = new ReadingListItem("Anne of Green Gables","Ella Tomlinson","romance",5);
+
+        readingList.put("Anne of Green Gables", item1);
+
+        String outputString = String.valueOf(ReadingTrackerMain.listInfoView(readingList));
+
+        assertEquals("""
+                You have 1 book in your Reading List, it is:
+                TITLE: Anne of Green Gables
+                AUTHOR: Ella Tomlinson
+                GENRE: romance
+                INTEREST: 5
+                ----------------------------------------
+
+                """, outputString);
+
+    }
+
+    @Test
+    void listInfoViewMultiBooks() {
+
+        HashMap<String, ReadingListItem> readingList = new HashMap<>();
+
+        ReadingListItem item1 = new ReadingListItem("Anne of Green Gables","Ella Tomlinson","romance",5);
+        ReadingListItem item2 = new ReadingListItem("Harry Potter","J.K Rowling","fantasy",7);
+        ReadingListItem item3 = new ReadingListItem("Lord of the Rings","J.R.R Tolkien","classics",8);
+
+        readingList.put("Anne of Green Gables", item1);
+        readingList.put("Harry Potter", item2);
+        readingList.put("Lord of the Rings", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.listInfoView(readingList));
+
+        assertEquals("""
+                You have 3 books in your Reading List, they are:
+                TITLE: Harry Potter
+                AUTHOR: J.K Rowling
+                GENRE: fantasy
+                INTEREST: 7
+                ----------------------------------------
+                                
+                TITLE: Anne of Green Gables
+                AUTHOR: Ella Tomlinson
+                GENRE: romance
+                INTEREST: 5
+                ----------------------------------------
+                                
+                TITLE: Lord of the Rings
+                AUTHOR: J.R.R Tolkien
+                GENRE: classics
+                INTEREST: 8
+                ----------------------------------------
+                
+                """, outputString);
+
+    }
+
 
     @Test
     void allInfoView() {
