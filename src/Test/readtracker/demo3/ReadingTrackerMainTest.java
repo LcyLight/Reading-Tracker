4d31a2abe1970fb31cc9bfa89af1ad2386621a3c
yiting.ouyang <yiting.ouyang@cs.ucalgary.ca>
2022-04-13 04:53:00 +0000

    finished tests for month stats

--- src/Test/readtracker/demo3/ReadingTrackerMainTest.java
+++ src/Test/readtracker/demo3/ReadingTrackerMainTest.java
@@ -577,9 +577,301 @@ class ReadingTrackerMainTest {
     }
 
     @Test
-    void stringMonthStats() {
+    void stringMonthStatsJan() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "January";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "January", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","January",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","March",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of January are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
     }
 
+    @Test
+    void stringMonthStatsFeb() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "February";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "February", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","February",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","March",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of February are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsMar() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "March";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "March", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","March",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of March are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsApr() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "April";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "April", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","April",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of April are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsMay() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "May";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "May", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","May",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of May are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsJun() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "June";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "June", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","June",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of June are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsJuly() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "July";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "July", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","July",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of July are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsAug() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "August";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "August", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","August",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of August are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsSep() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "September";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "September", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","September",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of September are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsOct() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "October";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "October", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","October",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of October are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsNov() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "November";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "November", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","November",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of November are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsDec() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "December";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "December", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","December",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of December are:
+                Diary of a Wimpy Kid
+                Goodnight Moon
+                This is a total of 2 books, and 734 pages read!""", outputString);
+    }
+
+    @Test
+    void stringMonthStatsNoBooks() {
+
+        HashMap<String, BookLogItem> bookLog = new HashMap<>();
+        String months = "January";
+
+        BookLogItem item1 = new BookLogItem("Goodnight Moon", "Tim Orwell", "May", 2, 534, "fantasy");
+        BookLogItem item2 = new BookLogItem("Diary of a Wimpy Kid","Jeff Kinney","May",2,200,"mystery");
+        BookLogItem item3 = new BookLogItem("Hitchhiker's Guide to the Galaxy","Douglas Adams","February",3,700,"sci-fi");
+
+        bookLog.put("Goodnight Moon", item1);
+        bookLog.put("Diary of a Wimpy Kid", item2);
+        bookLog.put("Hitchhiker's Guide to the Galaxy", item3);
+
+        String outputString = String.valueOf(ReadingTrackerMain.stringMonthStats(bookLog, months));
+
+        assertEquals("""
+                The books you finished in the month of January are:
+                This is a total of 0 books, and 0 pages read!""", outputString);
+    }
     @Test
     void stringGenreStats() {
     }
